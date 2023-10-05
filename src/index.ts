e6649dc76c46db346128c0c4bc0ea48bbaf96599
larabr <7375870+larabr@users.noreply.github.com>
2023-10-05 10:22:26 +0200

    Export Brainpool curves, and export all curves through single entrypoint
    All curves are needed in OpenPGP.js, so importing them separately does not have advantages.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-throw new Error('Incorrect usage. Import submodules instead');
+export { ed448 } from "./ed448.js";
+// export { ed25519 } from "./ed25519.js";
+export { p256 } from "./p256.js";
+export { p384 } from "./p384.js";
+export { p521 } from "./p521.js";
+export { secp256k1 } from "./secp256k1.js";
+export { brainpoolP256r1 } from "./brainpoolP256r1.js";
+export { brainpoolP384r1 } from "./brainpoolP384r1.js";
+export { brainpoolP512r1 } from "./brainpoolP512r1.js";
